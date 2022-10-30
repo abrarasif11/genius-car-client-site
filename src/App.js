3f156dc89b45194bd218b47646c17c3761344c01
abrarasif11 <faasif929@gmail.com>
2022-10-31 00:19:07 +0600

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Main from './Layout/Main';
+import Home from './Page/Home/Home/Home';
+import Login from './Page/Login/Login';
 
 function App() {
+  const router = createBrowserRouter([
+   {
+    path: '/',
+    element : <Main></Main>,
+    children : [
+      {
+        path: '/',
+        element : <Home></Home>
+      },
+      {
+        path: '/login',
+        element : <Login></Login>
+      }
+    ]
+   }
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='max-w-screen-xl mx-auto'>
+    <RouterProvider router={router}></RouterProvider> 
     </div>
   );
 }
